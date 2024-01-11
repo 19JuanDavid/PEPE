68d8fe25c9bb2f0505da248aa3ae7691e7b87f72
Juan David <Juan David@DESKTOP-0RCH09K>
2024-01-11 15:55:34 +0100

    Inicializacion de nueva aplicacion

---
+++ src/gitprueba/GitPrueba.java
@@ -0,0 +1,17 @@
+
+package gitprueba;
+
+/**
+ *
+ * @author Juan David
+ */
+public class GitPrueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(" Codigo viene de local ");
+    }
+    
+}
